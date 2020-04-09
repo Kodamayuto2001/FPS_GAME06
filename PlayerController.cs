d2a18f6bbb5daaa0f1209c687c10dfa5be938190
kodamayuto <yuto2001gyokugan@gmail.com>
2020-04-09 11:49:25 +0900

    ok

---
+++ PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float _moX;
+    private float _moZ;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _moX = Input.GetAxisRaw("Horizontal") * Time.deltaTime;
+        _moZ = Input.GetAxisRaw("Vertical") * Time.deltaTime;
+
+    }
+}
